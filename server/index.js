568c4300f1e457fb05e36f63f3782a24c9737fc0
mbayanirad <m.baianerad@gmail.com>
2022-08-08 13:29:53 -0400

    routes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
 const express = require('express')
-const PORT = 8000
+const PORT = process.env.PORT || 4000;
+
+
+
+//load config
+require("dotenv").config({path:"./config/config.env"})
+
 
 const app = express()
 
-.get('/', (req, res) => {
+//logging 
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan('env'))
+}
+app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server runing in ${process.env.NODE_ENV} mode on port ${PORT}`)
 })
