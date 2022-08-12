f5329851fbe63664ff9209c8f428ac0884f94427
mbayanirad <m.baianerad@gmail.com>
2022-08-12 11:25:53 -0400

    event details

---
+++ server/utils/getPositionFromAddress.js
@@ -0,0 +1,27 @@
+// Exercise 3.2 - `getAddressPosition`
+// ---------------------------------
+
+const opencage = require('opencage-api-client');
+require('dotenv').config();
+
+const getPositionFromAddress = (address) => {
+  const requestObj = {
+    key: process.env.OPENCAGE_API_KEY,
+    q: address,
+  };
+  return opencage
+  .geocode(requestObj)
+  .then((data) => {
+    return (data.results[0].geometry);
+  })
+  .catch((error) => {
+    console.log('error', error.message);
+  });
+
+
+  // return something...
+};
+
+// getPositionFromAddress(
+//   '301 Rue Caisse, Verdun, QC H4G 3M3, Canada'
+// ).then((response) => console.log(response));
